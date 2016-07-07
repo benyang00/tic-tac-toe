19f6c5d0dbf50b4f331b2ecf60220b410f62ed5c
ben yang <benyang00@gmail.com>
2016-07-07 15:47:38 +0800

    added layout version of tic tac toe

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//Global variables
+var wCombo = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9]
+];
+var xName;
+var oName;
+var gFilled = 0;
+var turns;
+
+document.querySelector('#reg #start').addEventListener("click", function(gStart) {
+  document.querySelector('#reg #start').innerText = 'Restart Game';
+}, false);
+
+function pTurns() {}
+
+function gridClicked() {}
+
+function wCheck() {}
+
+function wTally() {}
+
+function reStart() {}
+
+function rsScores() {}
